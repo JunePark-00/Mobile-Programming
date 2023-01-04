dd310ebb9b3199f66fe7e2993f7197c42c338114
June <81201633+JunePark-00@users.noreply.github.com>
2023-01-04 20:50:24 +0900

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,37 @@
+package kr.ac.dankook.app11_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.*
+import android.util.Log
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myHandler = object : Handler(Looper.getMainLooper()) {
+            override fun handleMessage(msg: Message) {
+                super.handleMessage(msg)
+                val helloText = findViewById<TextView>(R.id.helloText)
+                Log.d("BkgThread","Main thread")
+                if (msg.what == 1) {
+                    helloText.setText("${msg.arg1}")
+                }
+            }
+        }
+
+        Thread {
+            var i = 0
+            while(i<10){
+                i += 1
+                Log.d("BkgThread","In background thread : $i")
+                var msg = myHandler.obtainMessage()
+                msg.what = 1
+                msg.arg1 = i
+                myHandler.sendMessage(msg)
+                Thread.sleep(1000)
+            }
+        }.start()
+    }
+}
